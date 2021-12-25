7c5ce05cb0418d61bd1cbe1a70c78e4b1e171cb8
Migan178 <tigersiwoo131@gmail.com>
2021-12-25 20:37:36 +0900

    타임아웃 빼고 전부 완성... 인가?

---
+++ src/Commands/관리.js
@@ -0,0 +1,171 @@
+const { SlashCommand, Slash } = require('discommand-slash')
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { CommandInteraction } = require('discord.js')
+
+module.exports = class extends SlashCommand {
+  data = new SlashCommandBuilder()
+    .setName('관리')
+    .setDescription('관리기능입니다.')
+    .addStringOption(options =>
+      options
+        .setName('항목')
+        .setDescription('항목을 고릅니다.')
+        .setRequired(true)
+        .addChoice('킥', '킥')
+        .addChoice('밴', '밴')
+        .addChoice('청소', '청소')
+        .addChoice('슬로우모드', '슬로우모드')
+        .addChoice('타임아웃', '타임아웃')
+    )
+    .addMentionableOption(options => options.setName('유저').setRequired(false))
+    .addStringOption(options => options.setName('이유').setRequired(false))
+    .addNumberOption(options => options.setName('갯수').setRequired(false))
+    .addNumberOption(options => options.setName('초').setRequired(false))
+  /**
+   *
+   * @param {CommandInteraction} interaction
+   * @param {Slash} slash
+   */
+  execute(interaction, slash) {
+    const 항목 = interaction.options.getString('항목')
+
+    if (항목 === '킥') {
+      if (
+        !interaction.guild.members.cache
+          .get(interaction.author.id)
+          .permissions.has('KICK_MEMBERS')
+      )
+        return interaction.reply({
+          content: '님에게 킥하기 권한이 없습니다.',
+          ephemeral: true,
+        })
+
+      if (!interaction.guild.me.permissions.has('KICK_MEMBERS'))
+        return interaction.reply({
+          content: '제게 킥하기 권한이 없습니다.',
+          ephemeral: true,
+        })
+      const 유저 = interaction.options.getMentionable('유저')
+      const 이유 = interaction.options.getString('이유')
+
+      if (!유저.kickable)
+        return interaction.reply({
+          content: '유저를 킥할 수 없습니다.',
+          ephemeral: true,
+        })
+
+      try {
+        유저.kick(이유)
+        interaction.reply({
+          content: `${유저.tag}님을 킥했습니다.`,
+          ephemeral: true,
+        })
+      } catch (error) {
+        console.error(error)
+        return interaction.reply({
+          content: '오류가 발생했습니다.\n오류내용: ' + error.message,
+          ephemeral: true,
+        })
+      }
+    } else if (항목 === '밴') {
+      if (
+        !interaction.guild.members.cache
+          .get(interaction.author.id)
+          .permissions.has('BAN_MEMBERS')
+      )
+        return interaction.reply({
+          content: '님에게 밴하기 권한이 없습니다.',
+          ephemeral: true,
+        })
+
+      if (!interaction.guild.me.permissions.has('BAN_MEMBERS'))
+        return interaction.reply({
+          content: '제게 밴하기 권한이 없습니다.',
+          ephemeral: true,
+        })
+      const 유저 = interaction.options.getMentionable('유저')
+      const 이유 = interaction.options.getString('이유')
+
+      if (!유저.banable)
+        return interaction.reply({
+          content: '유저를 킥할 수 없습니다.',
+          ephemeral: true,
+        })
+
+      try {
+        유저.ban({ reason: 이유 })
+        interaction.reply({
+          content: `${유저.tag}님을 밴했습니다.`,
+          ephemeral: true,
+        })
+      } catch (error) {
+        console.error(error)
+        return interaction.reply({
+          content: '오류가 발생했습니다.\n오류내용: ' + error.message,
+          ephemeral: true,
+        })
+      }
+    } else if (항목 === '청소') {
+      if (
+        !interaction.guild.members.cache
+          .get(interaction.author.id)
+          .permissions.has('MANAGE_MESSAGES')
+      )
+        return interaction.reply({
+          content: '님에게 메세지 관리하기 권한이 없습니다.',
+          ephemeral: true,
+        })
+
+      if (!interaction.guild.me.permissions.has('MANAGE_MESSAGES'))
+        return interaction.reply({
+          content: '제게 메세지 관리하기 권한이 없습니다.',
+          ephemeral: true,
+        })
+      await interaction.channel?.messages
+        .fetch({
+          limit: interaction.options.getNumber('갯수'),
+        })
+        .then(messages => {
+          interaction.guild?.channels.fetch(interaction.channelId).then(a => {
+            a.bulkDelete(messages)
+            interaction.reply(
+              `채팅 ${interaction.options.getNumber('갯수')}개를 청소했습니다.`
+            )
+          })
+        })
+        .catch(err => {
+          interaction.reply({
+            content: `오류가 발생했습니다.\n오류내용: ${err.message}`,
+            ephemeral: true,
+          })
+        })
+    } else if (항목 === '슬로우모드') {
+      if (
+        !interaction.guild.members.cache
+          .get(interaction.user.id)
+          .permissions.has('MANAGE_CHANNELS')
+      )
+        return interaction.reply({
+          content: '님에게 채널 관리하기 권한이 없습니다.',
+          ephemeral: true,
+        })
+
+      if (!interaction.guild.me.permissions.has('MANAGE_CHANNELS'))
+        return interaction.reply({
+          content: '제게 채널 관리하기 권한이 없습니다.',
+          ephemeral: true,
+        })
+
+      interaction.channel.setRateLimitPerUser(
+        interaction.options.getNumber('초')
+      )
+
+      interaction.reply(
+        `채널의 슬로우모드를 ${interaction.options.getNumber(
+          '초'
+        )}초로 설정했습니다.`
+      )
+    } else if (항목 === '타임아웃') {
+    }
+  }
+}
